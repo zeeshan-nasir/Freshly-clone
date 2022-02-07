4ffe5b5ec6af325a73ac48d8f31d34a263e795ba
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-02-07 13:51:11 +0530

    deleted last change

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,3 @@
-let userData = JSON.parse(localStorage.getItem("userData")) || [];
-
 document.querySelector("#loginBtn").addEventListener("click", () => {
     var email = document.querySelector("#email").value;
     var password = document.querySelector("#password").value;
@@ -12,13 +10,6 @@ document.querySelector("#loginBtn").addEventListener("click", () => {
     }
     else if (email !== "" && password !== "") {
         window.location.href = "plan.html";
-        let obj = {
-            email: email,
-            password: password
-        }
-        obj.push(userData);
-        localStorage.setItem("userData",JSON.stringify(userData));
-        window.location.href = "otp.html";
     }
 });
 
